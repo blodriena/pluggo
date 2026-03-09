852688294e42ab33e35a70524b920572c6d51986
charos_7 <toiirovacharos@gmail.com>
2026-03-10 00:06:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const navbar = document.querySelector(".navbar");
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll(".nav-links li");
+
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 80){
+    navbar.classList.add("scrolled");
+  } else{
+    navbar.classList.remove("scrolled");
+  }
+
+  let current = "";
+
+  sections.forEach(section=>{
+    const sectionTop = section.offsetTop - 150;
+    const sectionHeight = section.clientHeight;
+
+    if(scrollY >= sectionTop){
+      current = section.getAttribute("id");
+    }
+  });
+
+  navItems.forEach(li=>{
+    li.classList.remove("active");
+
+    if(li.querySelector("a").getAttribute("href") === "#" + current){
+      li.classList.add("active");
+    }
+  });
+
+});
